7b0614fedc7aa791b048d70a73765a08aa940024
alvaro <alvarodevejer4@gmail.com>
2024-11-18 10:18:56 +0100

    .env

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000
+
 
 mongoose.connect("mongodb://localhost:27017/juegos", {useNewUrlParser: true, useUnifiedTopology: true})
 
